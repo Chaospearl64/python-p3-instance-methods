47b3f1969534e2daf17bd5456487a61fe14031d8
Cody Cogbill <codeman24@gmail.com>
2023-05-24 09:47:22 -0600

    done

--- lib/dog.py
+++ lib/dog.py
@@ -3,5 +3,8 @@
 class Dog:
     # Class body goes here
 
-    #Instance method definition
-    pass
+    def sit(self):
+        print("The dog is sitting.")
+    
+    def bark(self):
+        print("Woof!")
